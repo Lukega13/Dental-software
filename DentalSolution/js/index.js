638639dd782360af59ac037d53e68a9849b34be0
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2020-08-04 15:57:20 -0300

    Atualizações

--- DentalSolution/js/index.js
+++ DentalSolution/js/index.js
@@ -1,5 +1,5 @@
 
-function acionarEmail(){
+function acionarEmail() {
     var email = document.getElementById('email');
     email.classList.remove('erro-input')
     email.classList.add('input-acionado')
@@ -9,7 +9,7 @@ function acionarEmail(){
     text.classList.add('tittle-email-active')
 }
 
-function acionarSenha(){
+function acionarSenha() {
     var senha = document.getElementById('senha');
     senha.classList.remove('erro-input')
     senha.classList.add('input-acionado')
@@ -20,32 +20,32 @@ function acionarSenha(){
 }
 
 
-function fillEmail(){
+function fillEmail() {
     var email = document.getElementById('email');
     var text = document.getElementById('tittle-email')
-    
-    if(email.value.length < 3) {
+
+    if (email.value.length < 3) {
         email.classList.remove('input-acionado')
         email.classList.add('erro-input')
 
         text.classList.remove('tittle-email-active')
         text.classList.add('tittle-email-error')
-        }else {
-            acionarEmail()
-        }
+    } else {
+        acionarEmail()
+    }
 }
 
-function fillSenha(){
+function fillSenha() {
     var senha = document.getElementById('senha');
     var text = document.getElementById('tittle-senha')
 
-    if(senha.value.length < 3) {
+    if (senha.value.length < 3) {
         senha.classList.remove('input-acionado')
         senha.classList.add('erro-input')
 
         text.classList.remove('tittle-senha-active')
         text.classList.add('tittle-senha-error')
-        }else {
+    } else {
         acionarSenha()
-        }
+    }
 }
