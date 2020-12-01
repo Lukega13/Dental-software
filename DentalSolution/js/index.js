920c91ba44e7627095ef417888afc12bb5727641
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2020-12-01 14:35:49 -0300

    Atualization

--- DentalSolution/js/index.js
+++ DentalSolution/js/index.js
@@ -176,7 +176,6 @@ const renderCalendar = () => {
 
     document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-    document.querySelector(".date p").innerHTML = new Date().toDateString();
 
     let days = "";
 
